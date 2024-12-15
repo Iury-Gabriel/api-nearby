6e642a5b29d5ba44f046ea219232dcc95949830c
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-12-15 15:20:00 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
@@ -7,6 +8,7 @@ const PORT = 3333
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.use(errorHandling)
